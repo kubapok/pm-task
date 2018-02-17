fcd71c11f975f836eff346d10186610ae8c3fdde
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-02-17 17:40:01 +0100

    generalize week time series plot to function taking startdate and endate as parameters

--- program.py
+++ program.py
@@ -4,13 +4,13 @@ import datetime
 import seaborn as sns
 sns.set(color_codes=True)
 
-str_weekday = {1: 'Monday',
-               2: 'Tuesday',
-               3: 'Wendsday',
-               4: 'Thursday',
-               5: 'Friday',
-               6: 'Saturday',
-               7: 'Sunday'}
+str_weekday = {0: 'Monday',
+               1: 'Tuesday',
+               2: 'Wendsday',
+               3: 'Thursday',
+               4: 'Friday',
+               5: 'Saturday',
+               6: 'Sunday'}
 
 
 report = pd.read_csv('PRSA_data_2010.1.1-2014.12.31.csv', sep=',')
@@ -20,25 +20,25 @@ r['datetime'] = pd.Series([datetime.datetime(y, m, d, h)
                            for y, m, d, h in zip(r.year, r.month, r.day, r.hour)])
 r['weekday'] = pd.Series([x.weekday() for x in r['datetime']])
 
-r2012 = r[r.year == 2012]
+
+def plot_days(r, start_date, end_date):
+    plot_days_report = r[(r.datetime > start_date) & (r.datetime < end_date)]
+
+    timedelta = end_date - start_date
+    plt.locator_params(numticks=timedelta.days)
+    ax = sns.tsplot(plot_days_report['pm2.5'])
+    ax.set(xticks=[24 * i for i in range(timedelta.days)])
+    ax.set_xticklabels([str_weekday[(start_date.weekday() + i) % 7] + ' 0h'
+                        for i in range(timedelta.days)], rotation=45, fontsize=8)
+    ax.set_xlabel('Day of week')
+    ax.set_ylabel('PM2.5 concentration (ug/m^3) ')
+    plt.title(str(start_date.year) + ' ' + start_date.strftime('%b') +
+              ' ' + str(start_date.day) + ' (' + start_date.strftime('%a') + ') - ' + 
+    str(end_date.year) + ' ' + end_date.strftime('%b') +
+              ' ' + str(end_date.day) + ' (' + end_date.strftime('%a') + ')')
+    plt.show()
 
 
-r20120101_01_07 = r[(r.year == 2012) & (r.month == 1)
-                    & (2 <= r.day) & (r.day < 8)]
-r20120101_01_07 = r[(r.year == 2010) & (r.month == 1)
-                    & (1 <= r.day) & (r.day < 7)]
-plt.locator_params(numticks=7)
-ax = sns.tsplot(r20120101_01_07['pm2.5'])
-ax.set(xticks=[24 * i for i in range(7)])  # $, xticklabels=list(range(7)))
-ax.set_xticklabels(['Monday 0h', 'Tuesday 0h', 'Wendsday 0h',
-                    'Th', 'F', 'Sa', 'Su'], rotation=45, fontsize=8)
-ax.set_xlabel('Day of week')
-ax.set_ylabel('PM2.5 concentration (ug/m^3) ')
-plt.title('January 2012, Days: 02(Mon) - 08 (Sun)')
-plt.show()
-
-
-
-
+plot_days(r, datetime.datetime(2012, 1, 1), datetime.datetime(2012, 1, 15))
 
 # sns.tsplot(r20120101_01_07['pm2.5'], time = r20120101_01_07['datetime'])
