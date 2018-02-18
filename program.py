48a10c151e033989165aab4af884b02068fa8f6c
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-02-18 01:40:55 +0100

    add legend to month avarages plots

--- program.py
+++ program.py
@@ -35,6 +35,10 @@ def get_means_in_months(report, year):
     return month_means
 
 
+def plot_months():
+    pass
+
+
 if __name__ == "__main__":
     sns.set(color_codes=True)
     report = get_report()
@@ -43,14 +47,17 @@ if __name__ == "__main__":
 
     pal = sns.color_palette()[:5]
     plt.locator_params(numticks=12)
+    legend_col = []
     for year, color in zip(range(2010,2015),pal):
         ax = sns.tsplot(get_means_in_months(report, year), color = color)
         ax.legend(color)
+        legend_col.append(mpatches.Patch(color = color, label = 'year ' + str(year)))
 
-    red_patch = mpatches.Patch(color='red', label='The red data', linewidth = 0.01)
-    blue_patch = mpatches.Patch(color='blue', label='The blue data')
+    # red_patch = mpatches.Patch(color='red', label='The red data')
+    # blue_patch = mpatches.Patch(color='blue', label='The blue data')
 
-    plt.legend(handles=[red_patch, blue_patch])
+    # plt.legend(handles=[red_patch, blue_patch])
+    plt.legend(handles=legend_col)
 
     ax.set(xticks=[i for i in range(12)])
     ax.set_xticklabels([datetime.datetime(2000,i,1).strftime("%B") for i in range(1,13)], rotation=45, fontsize=8)
