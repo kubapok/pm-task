97049ef1fb2de8acd367ebc7d60b19c0f1698c54
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-02-18 01:45:21 +0100

    refactor month plots

--- program.py
+++ program.py
@@ -31,12 +31,25 @@ def plot_days(report, start_date, end_date):
 def get_means_in_months(report, year):
     report_year = report[report.year == year]
     month_means = pd.Series([report_year[report_year['month'] == x]['pm2.5'].mean()
-                   for x in range(1, 13)])
+                             for x in range(1, 13)])
     return month_means
 
 
-def plot_months():
-    pass
+def plot_months(report,):
+    pal = sns.color_palette()[:5]
+    plt.locator_params(numticks=12)
+    legend_col = []
+    for year, color in zip(range(2010, 2015), pal):
+        ax = sns.tsplot(get_means_in_months(report, year), color=color)
+        ax.legend(color)
+        legend_col.append(mpatches.Patch(
+            color=color, label='year ' + str(year)))
+    plt.legend(handles=legend_col)
+    ax.set(xticks=[i for i in range(12)])
+    ax.set_xticklabels([datetime.datetime(2000, i, 1).strftime("%B")
+                        for i in range(1, 13)], rotation=45, fontsize=8)
+    ax.set_ylabel('PM2.5 concentration (ug/m^3) ')
+    plt.show()
 
 
 if __name__ == "__main__":
@@ -45,24 +58,6 @@ if __name__ == "__main__":
     report.isnull().sum()
     report.describe()
 
-    pal = sns.color_palette()[:5]
-    plt.locator_params(numticks=12)
-    legend_col = []
-    for year, color in zip(range(2010,2015),pal):
-        ax = sns.tsplot(get_means_in_months(report, year), color = color)
-        ax.legend(color)
-        legend_col.append(mpatches.Patch(color = color, label = 'year ' + str(year)))
-
-    # red_patch = mpatches.Patch(color='red', label='The red data')
-    # blue_patch = mpatches.Patch(color='blue', label='The blue data')
-
-    # plt.legend(handles=[red_patch, blue_patch])
-    plt.legend(handles=legend_col)
-
-    ax.set(xticks=[i for i in range(12)])
-    ax.set_xticklabels([datetime.datetime(2000,i,1).strftime("%B") for i in range(1,13)], rotation=45, fontsize=8)
-    plt.show()
-    # ax.set(xticks=[24 * i for i in range(timedelta.days)])
-
-    # plot_months(report, plot_months
-    #           datetime.datetime(2013, 1))
+    plot_days(report, datetime.datetime(2012, 1, 1),
+              datetime.datetime(2012, 1, 8))
+    plot_months(report)
