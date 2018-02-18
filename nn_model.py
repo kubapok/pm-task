9a138c41ba5cd6c58c4712d9f7d64488084160e8
Jakub Pokrywka <jakubpokrywka@gmail.com>
2018-02-18 23:25:49 +0100

    tune parameters

--- nn_model.py
+++ nn_model.py
@@ -1,35 +1,32 @@
-import pandas
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.wrappers.scikit_learn import KerasRegressor
-from sklearn.preprocessing import StandardScaler
-from sklearn.pipeline import Pipeline
-from analyze import get_report_cbwd_encoded
-import sklearn
 from sklearn.metrics import mean_squared_error
 from sklearn.model_selection import train_test_split
+from analyze import get_report_cbwd_encoded
 
 report = get_report_cbwd_encoded().dropna()
-data = pandas.DataFrame(report, columns=[
-                           'month', 'day', 'hour', 'DEWP', 'TEMP', 'PRES', 'Iws', 'Is', 'Ir',
-                           'is_cbwd_cv', 'is_cbwd_NW', 'is_cbwd_SE', 'is_cbwd_NE'])
-target = pandas.DataFrame(report, columns=['pm2.5'])
+data = pd.DataFrame(report, columns=[
+    'month',  'hour', 'DEWP', 'TEMP', 'PRES', 'Iws', 'Is', 'Ir',
+    'is_cbwd_cv', 'is_cbwd_NW', 'is_cbwd_SE', 'is_cbwd_NE'])
+target = pd.DataFrame(report, columns=['pm2.5'])
 X_train, X_test, y_train, y_test = train_test_split(
     data, target, test_size=0.1, random_state=0)
 
-scaler = sklearn.preprocessing.MinMaxScaler()
+scaler = MinMaxScaler()
 scaler.fit(X_train)
-X_trained_scaled = pandas.DataFrame(scaler.transform(X_train))
-
-X_trained_scaled = X_trained_scaled.values
-y_train = y_train.values
-X = X_trained_scaled[:, 0:13]
-Y = y_train[:, 0]
+X_train_scaled = pd.DataFrame(
+    scaler.transform(X_train)).values[:, 0:X_train.shape[1]]
+y_train = y_train.values[:, 0]
 
 
 # def baseline_model():
 #     model = Sequential()
 #     model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
 #     model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
 #     model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
 #     model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
@@ -40,34 +37,50 @@ Y = y_train[:, 0]
 #     return model
 
 
+# estimator = KerasRegressor(build_fn=baseline_model,
+#                            nb_epoch=10, batch_size=1, verbose=1)
+
+# def baseline_model():
+#     model = Sequential()
+#     model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(100, input_dim=40, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(500, input_dim=500, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(500, input_dim=500, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(100, input_dim=40, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(40, input_dim=20, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(1, kernel_initializer='normal'))
+#     model.compile(loss='mean_squared_error', optimizer='adam')
+#     return model
+
+
+# estimator = KerasRegressor(build_fn=baseline_model,
+#                            nb_epoch=10, batch_size=1, verbose=1)
 def baseline_model():
     model = Sequential()
-    model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(40, input_dim=13, kernel_initializer='normal', activation='relu'))
-    model.add(Dense(20, input_dim=13, kernel_initializer='normal', activation='relu'))
+    model.add(Dense(20, input_dim=X_train.shape[1],
+                    kernel_initializer='normal', activation='relu'))
+    model.add(Dense(100, input_dim=40,
+                    kernel_initializer='normal', activation='relu'))
+    model.add(Dense(500, input_dim=500,
+                    kernel_initializer='normal', activation='relu'))
+    model.add(Dense(500, input_dim=500,
+                    kernel_initializer='normal', activation='relu'))
+    model.add(Dense(100, input_dim=40,
+                    kernel_initializer='normal', activation='relu'))
+    model.add(Dense(40, input_dim=20,
+                    kernel_initializer='normal', activation='relu'))
     model.add(Dense(1, kernel_initializer='normal'))
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
 
 estimator = KerasRegressor(build_fn=baseline_model,
-                           nb_epoch=1000, batch_size=5, verbose=1)
-# estimator = KerasRegressor(build_fn=baseline_model,
-#                            nb_epoch=1000, batch_size=5, verbose=1)
+                           nb_epoch=10, batch_size=1, verbose=1)
+estimator.fit(X_train_scaled, y_train)
 
-estimator.fit(X, Y)
+X_test_scaled = pd.DataFrame(
+    scaler.transform(X_test)).values[:, 0:X_train.shape[1]]
+y_test = y_test.values[:, 0]
 
-
-X_test_scaled = pandas.DataFrame(scaler.transform(X_test))
-X_test_scaled = X_test_scaled.values
-y_test = y_test.values
-X = X_test_scaled[:, 0:13]
-Y = y_test[:, 0]
-
-predictions = estimator.predict(X)
-print(mean_squared_error(Y, predictions))
+predictions = estimator.predict(X_test_scaled)
+print(mean_squared_error(y_test, predictions))
